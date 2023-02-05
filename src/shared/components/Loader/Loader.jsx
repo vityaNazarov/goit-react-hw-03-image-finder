79017d88da0b6226b02fdfdda9c2a33d99acc6f4
Viktor <108054291+vityaNazarov@users.noreply.github.com>
2023-02-05 15:48:35 +0200

    new spinner

--- src/shared/components/Loader/Loader.jsx
+++ src/shared/components/Loader/Loader.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
 
-import { ThreeDots } from 'react-loader-spinner';
+import { RotatingLines } from 'react-loader-spinner';
 
 import styles from '../Loader/Loader.module.css';
 
 const Loader = () => {
   return (
     <div className={styles.load_wrapper}>
-      <ThreeDots
-        height="80"
-        width="80"
-        radius="9"
-        color="red"
-        ariaLabel="three-dots-loading"
-        wrapperStyle={{}}
-        wrapperClassName=""
+      <RotatingLines
+        strokeColor="grey"
+        strokeWidth="5"
+        animationDuration="0.75"
+        width="96"
         visible={true}
       />
     </div>
