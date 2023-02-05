61258ff7783d847a2c2b3da9fe912d3c8bc010ac
Viktor <108054291+vityaNazarov@users.noreply.github.com>
2023-02-05 15:49:36 +0200

    777

--- src/shared/services/posts-api.js
+++ src/shared/services/posts-api.js
@@ -1,22 +1,22 @@
-import axios from "axios";
+import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "https://pixabay.com/api",
-    
-    params: {
-        image_type: 'photo',
-        orientation: 'horizontal', 
-        key : '30821762-b8f2171742f26b4a3279c46a9',
-    }
-})
+  baseURL: 'https://pixabay.com/api',
 
-export const fetchImages = async(q, page = 1)=> {
-    const {data} = await instance.get("/", {
-        params: {
-            q,
-            page,
-            per_page: 12,
-        }
-    });
-    return data;
-}
+  params: {
+    image_type: 'photo',
+    orientation: 'horizontal',
+    key: '32092680-3b49782323b5bf3e8b9e41587',
+  },
+});
+
+export const fetchImages = async (q, page = 1) => {
+  const { data } = await instance.get('/', {
+    params: {
+      q,
+      page,
+      per_page: 12,
+    },
+  });
+  return data;
+};
